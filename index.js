77c66021d4bf0278e204bd47ed3e3a474a3d2025
eccihxp <111105059+eccihxp@users.noreply.github.com>
2025-05-06 22:13:04 +0930

    Object and Variable conversion to FEN now works

--- index.js
+++ index.js
@@ -17,15 +17,25 @@ app.post('/run-function', (req, res) => {
     res.json({ message: 'Backend function executed successfully' }); // <-- JSON response
 });
 
-const pieceIDs = {
-    none: 0,
-    king: 1,
-    pawn: 2,
-    knight: 3,
-    bishop: 4,
-    rook: 5,
-    queen: 6,
+const pieceIDs = ["x", "k", "p", "n", "b", "r", "q"]
+//index of the piece name corresponds to the piece value which is added to the colour (8 for white, 16 for black) to identify piece
+
+let whiteActive = true
+//true if white turn, false if black turn
+
+let castlingRights = {
+    white: {kingside: true, queenside: true},
+    black: {kingside: true, queenside: true}
 }
+
+let enPassantTargetSquare = ""
+//present even if there is not a piece able to capture the en passant (leave blank if not applicable, square (eg "e3") otherwise)
+
+let halfmoveClock = 0
+//used for 50 move rule and FEN notation
+
+let fullmoveCount = 1
+//number of moves since start of game (starts at 1)
 
 let board = {
     a: [21, 19, 20, 22, 17, 20, 19, 21],
@@ -37,3 +47,52 @@ let board = {
     g: [10, 10, 10, 10, 10, 10, 10, 10],
     h: [13, 11, 12, 14, 9, 12, 11, 13]
 }
+
+function toFEN(input){
+    let outputFEN = ""
+    console.log("start")
+    
+    // Piece Placement
+    for(let i=0;i<8;i++){
+        let blankSpace = 0
+        for(let j=0;j<8;j++){
+            let workingSquare = input[String.fromCharCode(97+i)][j]
+            for(let k=0;k<7;k++){
+                if(workingSquare == 0){
+                    blankSpace += 1
+                    break
+                } else if(workingSquare%8 == k){
+                    if (blankSpace != 0){
+                        outputFEN += blankSpace
+                        blankSpace = 0
+                    }
+                    outputFEN += (workingSquare > 16 ? pieceIDs[k] : pieceIDs[k].toUpperCase())
+                }
+            }
+        }
+        if(blankSpace == 8){
+            outputFEN += blankSpace
+            blankSpace = 0
+        }
+        outputFEN += (i != 7 ? "/" : " ")
+    }
+
+    //Side to Move
+    outputFEN += (whiteActive == true ? "w " : "b ")
+
+    //Castling Ability
+    outputFEN += (castlingRights["white"]["kingside"] == false && castlingRights["white"]["queenside"] == false && castlingRights["black"]["kingside"] == false && castlingRights["black"]["queenside"] == false ? "- " : (castlingRights["white"]["kingside"] == true ? "K" : "") + (castlingRights["white"]["queenside"] == true ? "Q" : "") + (castlingRights["black"]["kingside"] == true ? "k" : "") + (castlingRights["black"]["queenside"] == true ? "q " : " "))
+
+    //En Passant Target Square
+    outputFEN += (enPassantTargetSquare != "" ? enPassantTargetSquare + " " : "- ")
+
+    //Halfmove Clock
+    outputFEN += halfmoveClock + " "
+
+    //Fullmove Counter
+    outputFEN += fullmoveCount
+
+    console.log(outputFEN)
+}
+
+toFEN(board)
