2d0fd345f73bbcad9b945f3bd9a960d756eab693
eccihxp <111105059+eccihxp@users.noreply.github.com>
2025-05-10 15:48:25 +0930

    Development on evaluation algorithm
    Adds weighting for different pawn structures (isolated, blocked, doubled). Minor fixes as well.

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ app.post('/makeMove', (req, res) => {
     console.log('Function triggered from frontend!');
     game.move((clickedSquare.toString() + req.body.moveMade.toString()).toString(), {sloppy: true})
     toBoardObject(game.fen())
+    assignObject()
     res.json({ message: theBoard}); // <-- JSON response
 });
 
@@ -226,21 +227,93 @@ function assignObject(){
 }
 
 function basicEval(){
+
+    //#region Piece Mobility
+    let activeMobility = game.moves().length //side to move
+    let opponentMobility = 0
+    for(let i=0;i<game.moves().length;i++){
+        game.move(game.moves()[i])
+        opponentMobility += game.moves().length
+        game.undo()
+    }
+    opponentMobility = opponentMobility/game.moves().length
+    console.log(activeMobility-opponentMobility)
+    //#endregion
+
     let whiteEval = 0
     let blackEval = 0
-
+    let filePawnsWhite = [0, 0, 0, 0, 0, 0, 0, 0]
+    let filePawnsBlack = [0, 0, 0, 0, 0, 0, 0, 0]
+    let doubledPawnsWhite = 0
+    let doubledPawnsBlack = 0
+    let blockedPawnsWhite = 0
+    let blockedPawnsBlack = 0
+    let isolatedPawnsWhite = 0
+    let isolatedPawnsBlack = 0
     for(let i=0;i<8;i++){
         for(let j=0;j<8;j++){
             if(theBoard[i][j] == 0){
                 //do nothing
             } else if(theBoard[i][j]/8 > 2){
                 blackEval += pointValue[theBoard[i][j]%8]
+                if(theBoard[i][j]%8 == 2){
+                    filePawnsBlack[j] += 1
+                    if(theBoard[i+1][j] != 0){
+                        blockedPawnsBlack += 1
+                    }
+                }
             }else{
                 whiteEval += pointValue[theBoard[i][j]%8]
+                if(theBoard[i][j]%8 == 2){
+                    filePawnsWhite[j] += 1
+                    if(theBoard[i-1][j] != 0){
+                        blockedPawnsWhite += 1
+                    }
+                }
             }
         }
     }
-    eval = (whiteEval - blackEval + (whiteActive==true ? 20 : -20))
+    for(let i=0;i<8;i++){
+        if(filePawnsBlack[i]>1){
+            doubledPawnsBlack += filePawnsBlack[i]
+        }
+        if(filePawnsBlack[i]>0){
+            if(i==0){
+                if(filePawnsBlack[i+1]==0){
+                    isolatedPawnsBlack += 1
+                }
+            } else if(i==7){
+                if(filePawnsBlack[i-1]==0){
+                    isolatedPawnsBlack += 1
+                }
+            } else{
+                if(filePawnsBlack[i-1]==0 && filePawnsBlack[i+1]==0){
+                    isolatedPawnsBlack += 1
+                }
+            }
+        }
+        if(filePawnsWhite[i]>1){
+            doubledPawnsWhite += filePawnsWhite[i]
+        }
+        if(filePawnsWhite[i]>0){
+            if(i==0){
+                if(filePawnsWhite[i+1]==0){
+                    isolatedPawnsWhite += 1
+                }
+            } else if(i==7){
+                if(filePawnsWhite[i-1]==0){
+                    isolatedPawnsWhite += 1
+                }
+            } else{
+                if(filePawnsWhite[i-1]==0 && filePawnsWhite[i+1]==0){
+                    isolatedPawnsWhite += 1
+                }
+            }
+        }
+    }
+    console.log(doubledPawnsBlack, doubledPawnsWhite, blockedPawnsBlack, blockedPawnsWhite, isolatedPawnsBlack, isolatedPawnsWhite)
+    console.log(whiteActive)
+    eval = ((whiteActive==true ? 20 : -20) + (whiteEval - blackEval) - (50 * (doubledPawnsWhite + blockedPawnsWhite + isolatedPawnsWhite - doubledPawnsBlack - blockedPawnsBlack - isolatedPawnsBlack)) + (whiteActive==true ? (activeMobility-opponentMobility)*10 : (activeMobility-opponentMobility)*-10))
     console.log(eval)
 }
 
